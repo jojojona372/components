18aed247a8426d4ee70054b810d31e83ecd30efd
Jona van der Pal <57063625+jojojona372@users.noreply.github.com>
2022-03-13 17:47:34 +0100

    Update subset of functions
    The updated functions now work with structures as inputs and outputs. I also added an example file to illustrate how to use the functions.

---
+++ components_example.m
@@ -0,0 +1,41 @@
+%% Example for using component functions to simulate a pump
+% Date: 13/03/2022
+% Author: Jona van der Pal
+
+clear
+clc
+
+% Flow names are not required, they are just here to make it easier to
+% understand.
+
+n(1).name = 'Flow 1';
+n(2).name = 'Flow 2s';
+n(3).name = 'Flow 2';
+
+comp(1).name = "Pump 1";
+comp(1).ef = 0.80;  % Isentropic efficiency
+
+% The input pressure is 0.1 bar
+n(1).p = 0.1;   % bar
+n(1).t = 20;    % °C
+n(1).h = XSteam('h_pt',n(1).p,n(1).t);
+n(1).s = XSteam('s_ph',n(1).p,n(1).h);
+
+% The output pressure is 10 bar
+n(3).p = 10;    % bar
+
+% This is the line that calculates the properties of the flow that comes
+% from the pump.
+% n is the structure that contains the flows.
+% 1,2,3 are the numbers of the ingoing, isentropic, and
+% outgoing flow in that order.
+% comp is the structure that contains the components
+% The last 1 is the component number of the pump
+[n,comp] = pump(n,1,2,3,comp,1);
+
+% This open the variable window so you can see all the properties that have
+% been calculated.
+open n
+
+disp("Pump work output: "+comp(1).wout+" kJ/kg.")
+disp(sprintf("As you can see, the work output is negative.\nThis is because it is a pump, so it requires a work input.")) %#ok<DSPS>
--- compressor.m
+++ compressor.m
@@ -1,26 +1,42 @@
 % Author: Jona van der Pal
 % Date: 13/03/2022
+% Version: 13032022.2
 
-function h = compressor(n1,n2,eff)
-    if ~isfield(n2,'p')
-        warning("Please declare the pressure of the outgoing flow before calling this function.");
+function [n,comp] = compressor(n,f1,f2s,f2,comp,nr)
+    %% This checks if the outgoing flow already has a pressure.
+    % If f2s has a pressure value but f2 does not.
+    if isfield(n(f2s),'p')&&~isempty(n(f2s).p)&&(~isfield(n(f2),'p')||isempty(n(f2).p))
+        n(f2).p = n(f2s).p;
+    % If f2 has a pressure value but f2s does not.
+    elseif isfield(n(f2),'p')&&~isempty(n(f2).p)&&(~isfield(n(f2s),'p')||isempty(n(f2s).p))
+        n(f2s).p = n(f2).p;
+    else
+        error("Please declare the pressure of the outgoing flow before calling this function.");
     end
-    if ~isfield(n1,'h')
-        if isfield(n1,'t')
-            n1.h = xa('h_t',n1.t);
-        elseif isfield(n1,'p')&&isfield(n1,'s')
-            n1.h = xa('h_ps',n1.p,n1,s);
+    if n(f2).p~=n(f2s).p
+        error("System is overdeterminate: The pressure of the isentropic outgoing flow is not the same as the pressure of the normal outgoing flow.")
+    end
+    %% 
+    if ~isfield(n(f1),'h')||isempty(n(f1).h)
+        if isfield(n(f1),'t')
+            n(f1).h = xa('h_t',n(f1).t);
+        elseif isfield(n(f1),'p')&&isfield(n(f1),'s')
+            n(f1).h = xa('h_ps',n(f1).p,n(f1),s);
         else
-            error("The enthalpy and the temperature and/or the pressure and/or the entropy are missing from the input stream.");
+            error("System is underdeterminate: The enthalpy is missing from the ingoing flow, and cannot be determined from the other flow properties.");
         end
     end
-    if ~isfield(n1,'s')
-        n1.s = xa('s_ph',n1.p,n1.h);
+    if ~isfield(n(f1),'s')
+        n(f1).s = xa('s_ph',n(f1).p,n(f1).h);
     end
-    n2s.s = n1.s;
-    n2s.h = xs('h_ps',n2.p,n2s.s);
-    h = (n2s.h-n1.h)/eff+n1.h;
-    if isnan(h)
+    n(f2s).s = n(f1).s;
+    n(f2s).h = xa('h_ps',n(f2).p,n(f2s).s);
+    n(f2).h = (n(f2s).h-n(f1).h)/comp(nr).ef+n(f1).h;
+    if isnan(n(f2).h)
         error("The enthalpy of the outgoing stream is NaN. Please calculate the enthalpy of the ingoing stream before calling this function.");
     end
+    n(f2).s = xa('s_ph',n(f2).p,n(f2).h);
+    n(f2).t = xa('t_h',n(f2).h);
+    % Calculating the work output
+    comp(nr).wout = n(f1).h-n(f2).h;
 end
